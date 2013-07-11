12f1e663eddfbd6002ad0c0a0f3ff2aace28fe3e
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-07-11 12:07:08 +0530

    Added code that computes number of FailoverLinks between 2 cluster nodes

--- graph-objects.js
+++ graph-objects.js
@@ -35,9 +35,36 @@ var vms_json = [
       primary_node: "node4.example.org"
     }
   },
+  {
+    pk: 5,
+    model: "ganeti_web.virtualmachine",
+    fields: {
+      secondary_node: "node2.example.org",
+      hostname: "instance5.example.org",
+      primary_node: "node3.example.org"
+    }
+  },
 ]
 
 VMGraph = {}
+FailoverLinks = {}
 vms_json.forEach(function(vm) {
-    VMGraph[vm["fields"]["hostname"]] = [vm["fields"]["primary_node"],vm["fields"]["secondary_node"]]
+    vm_hostname = vm["fields"]["hostname"]
+    pnode = vm["fields"]["primary_node"]
+    snode = vm["fields"]["secondary_node"]
+
+    // A HashMap object that will contain mapping from VM to pnode or snode for fast search.
+    VMGraph[vm_hostname] = [pnode,snode]
+
+    // FailoverLinks will contain number of failover possibilities b/n a PNode & an SNode.
+    try {
+      if (snode != null){
+        FailoverLinks[pnode][snode] += 1
+      }
+    }
+    catch(exception){
+        obj = {}
+        obj[snode] = 1
+        FailoverLinks[pnode] = obj
+    }
 });
