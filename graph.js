f5fa00dfaa3efb7ebcf6e2fda1d682b8d2a47cc6
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-07-13 20:52:42 +0530

    Panning using keys feature in progress

--- graph.js
+++ graph.js
@@ -194,3 +194,13 @@ function vertexSearch(e) {
     }
 }
 
+// Panning by pressing arrow keys (Work in progress)
+$("#cy").keypress(function (event) {
+  // handle cursor keys
+  if (event.keyCode == 37) {
+    // go left
+  } else if (event.keyCode == 39) {
+    // go right
+  }
+});
+
